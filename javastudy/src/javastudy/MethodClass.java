74dddef106a0c9aeeb8e8cccd71b3dd8adfc5693
30409 <30409@DESKTOP-SNASD8N>
2020-04-17 14:31:53 +0900

    0416

---
+++ javastudy/src/javastudy/MethodClass.java
@@ -0,0 +1,18 @@
+package javastudy;
+
+public class MethodClass {
+	//Call By Value 메소드
+	//매개변수가 기본형인 메소드
+	public static void callByValue(int value) {
+		System.out.println("메소드 내의 value: " + value);
+		value = value + 1;
+		System.out.println("메소드 내의 value: " + value);
+	}
+	//매개변수가 참조형인 메소드:Call By Reference
+	public static void callByReference(int [] ar) {
+		System.out.println("ar[0]: " + ar[0]);
+		ar[0] = ar[0] +1;
+		System.out.println("ar[0]: " + ar[0]);
+
+	}
+}
