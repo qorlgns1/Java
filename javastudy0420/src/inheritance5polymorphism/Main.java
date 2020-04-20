7ac0f7244e97a761a1449104c33a6871a7761c5f
qorlgns1 <rlgns0610@gmail.com>
2020-04-20 15:55:57 +0900

    0420-2

--- javastudy0420/src/inheritance5polymorphism/Main.java
+++ javastudy0420/src/inheritance5polymorphism/Main.java
@@ -3,6 +3,9 @@ package inheritance5polymorphism;
 public class Main {
 
 	public static void main(String[] args) {
+		//참조형 변수의 멤버 호출
+		//호출할 수 있는 것의 판단은 클래스를 가지고 판단
+		//오버라이딩 된 메소드는 생성자를 가지고 판단
 		Starcraft star = new Zerg();
 		star.attack();
 		
@@ -11,7 +14,7 @@ public class Main {
 		
 		star = new Terran();
 		star.attack();
-
+		
 	}
 
 }
