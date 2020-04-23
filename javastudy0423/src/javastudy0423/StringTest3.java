14c392d910a467e06692f73b896cdcec81b2b6d9
qorlgns1 <rlgns0610@gmail.com>
2020-04-23 18:26:00 +0900

    0423-1

--- javastudy0423/src/javastudy0423/StringTest3.java
+++ javastudy0423/src/javastudy0423/StringTest3.java
@@ -1,5 +1,7 @@
 package javastudy0423;
 
+import java.util.Arrays;
+
 public class StringTest3 {
 
 	public static void main(String[] args) {
@@ -35,6 +37,32 @@ public class StringTest3 {
 		// ar의 데이터를 순서대로 temp에 하나씩 대입
 		for (String temp : ar) {
 			System.out.println(temp);
+
+		}
+		int sum = 0;
+		for (String temp : ar) {
+			try {
+				sum = sum + Integer.parseInt(temp);
+			} catch (Exception e) {
+			}
+
+		}
+		System.out.println("합계:" + sum);
+		System.out.println("=====================");
+
+		String coupang = "0420,10000,0421,20000,0422,-,0423,14000";
+		// 위에 데이터는 날짜와 매출을 연속적으로 기록한 것임
+		// 위에 데이터를 가지고 매출의 평균을 구하시오.
+		// -는 매출이 없는 것이 아니라 영업을 하지 않은 경우이므로 제외하고 매출의 평균을 구하세요
+
+		// 1. ,단위로 분할해서 출력
+		// 2. 짝수번째 데이터만 출력
+		// 3. 짝수번째 데이터 평균을 구하기
+		
+		String[] arr = coupang.split(",");
+		System.out.println(arr.length);
+		for (int i = 1; i <= arr.length; i= i+2) {
+			 
 		}
 	}
 
