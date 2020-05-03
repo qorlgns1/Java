2d90032d5ed063acc16a46e970203dcaa055fd44
qorlgns1 <rlgns0610@gmail.com>
2020-05-03 11:49:25 +0900

    0503

---
+++ src/javayoutube/StringExercise.java
@@ -0,0 +1,14 @@
+package javayoutube;
+//코딩테스트
+//단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+class StringExercise{
+    String getMiddle(String word){
+
+        return word.substring((word.length()-1) / 2, word.length()/2 + 1);    
+    }
+    // 아래는 테스트로 출력해 보기 위한 코드입니다.
+    public static void  main(String[] args){
+        StringExercise se = new StringExercise();
+        System.out.println(se.getMiddle("power"));
+    }
+}
